3e774b8cc1b562e8b2de0d83f37936c349ec691b
Sam Chung <tieoneease@gmail.com>
2026-04-01 01:27:57 +0800

    task 6: screenshot module

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 export * from './manifest.js';
+export * from './screenshot.js';
 export * from './server.js';
 export { default as canvasVitePlugin } from './vite-plugin.js';
 export type { CanvasVitePluginOptions } from './vite-plugin.js';
